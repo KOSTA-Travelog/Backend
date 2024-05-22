e475f72ec8054cf7ed47cd5e6e63f608ff02b517
ssunnykku <ssunnykku@gmail.com>
2024-05-23 00:15:00 +0900

    [FIX] POST Query 수정

--- src/main/java/kosta/travelog/repository/Query.java
+++ src/main/java/kosta/travelog/repository/Query.java
@@ -6,7 +6,7 @@ public interface Query {
     String GET_PROFILE = "SELECT user_id,nickname, bio, profile_image, user_status FROM users WHERE user_id = ?";
 
     /*Posts + Post_images*/
-    String POST_LIST = "SELECT p.post_id, p.post_title, p.post_description, p.post_hashtag, p.post_date, p.post_status, p.user_id, i.image_id, i.images FROM Posts p INNER JOIN POST_IMAGES i on p.POST_ID = i.POST_ID WHERE post_status = 1 order by post_date desc";
+    String POST_LIST = "SELECT p.post_id, p.post_title, p.post_description, p.post_hashtag, p.post_date, p.post_status, p.user_id, i.image_id, i.images FROM Posts p LEFT OUTER JOIN POST_IMAGES i on p.POST_ID = i.POST_ID WHERE post_status = 1 order by post_date desc";
     String INSERT_POST_IMAGE = "INSERT INTO Post_images (image_id, images, post_id) VALUES (image_id.nextval, ?, ?)";
     String INSERT_POST = "INSERT INTO Posts (post_id, post_title, post_description, post_hashtag, post_date, post_status, user_id) VALUES (post_id.nextval, 'this is post title', 'this is post description', '#test', sysdate, 1, '235ddd1f-c51f-47bf-8aec-575220580504')";
     String UPDATE_POST_STATUS = "update posts set post_status = ? where post_id = ?";
