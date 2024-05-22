ef833e41e2c714d330665371202950874156bc49
ssunnykku <ssunnykku@gmail.com>
2024-05-22 17:47:02 +0900

    [fix] LoadProfileAction.java

--- src/main/java/kosta/travelog/servlet/action/LoadProfileAction.java
+++ src/main/java/kosta/travelog/servlet/action/LoadProfileAction.java
@@ -1,28 +1,34 @@
 package kosta.travelog.servlet.action;
 
-import java.io.IOException;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-
+import kosta.travelog.exception.DatabaseConnectException;
+import kosta.travelog.exception.DatabaseQueryException;
 import kosta.travelog.service.AccountService;
 import kosta.travelog.servlet.Action;
 import kosta.travelog.servlet.URLModel;
 import kosta.travelog.vo.UserVO;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
 public class LoadProfileAction implements Action {
 
-	@Override
-	public URLModel execute(HttpServletRequest request) throws ServletException, IOException {
-		String userId = request.getParameter("userId");
-		AccountService service= new AccountService();
-		UserVO profile = service.loadProfile(userId);
-		if(profile != null){
-			request.getSession().setAttribute("profile", profile);
+    @Override
+    public URLModel execute(HttpServletRequest request) throws ServletException, IOException, DatabaseConnectException {
+        String userId = request.getParameter("userId");
+        AccountService service = new AccountService();
+        UserVO profile = null;
+        try {
+            profile = service.loadProfile(userId);
+        } catch (DatabaseQueryException e) {
+            throw new RuntimeException(e);
+        }
+        if (profile != null) {
+            request.getSession().setAttribute("profile", profile);
 
-		}
-		
-		return null;
-	}
+        }
+
+        return new URLModel();
+    }
 
 }
