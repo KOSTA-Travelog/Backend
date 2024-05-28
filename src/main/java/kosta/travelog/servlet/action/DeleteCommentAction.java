fd20b3f129e3a555736d4111ba89c2d22ed834be
hellonuri <hellonuri.dvl@gmail.com>
2024-05-28 14:03:05 +0900

    CommentListAction, CountCommentAction, DeleteCommentAction, EditCommentAction 생성

--- src/main/java/kosta/travelog/servlet/action/DeleteCommentAction.java
+++ src/main/java/kosta/travelog/servlet/action/DeleteCommentAction.java
@@ -7,7 +7,9 @@ import javax.servlet.http.HttpServletRequest;
 
 import kosta.travelog.exception.DatabaseConnectException;
 import kosta.travelog.exception.DatabaseQueryException;
+import kosta.travelog.service.CommentService;
 import kosta.travelog.servlet.Action;
+import kosta.travelog.servlet.ResponseModel;
 import kosta.travelog.servlet.URLModel;
 
 public class DeleteCommentAction implements Action {
@@ -15,8 +17,17 @@ public class DeleteCommentAction implements Action {
 	@Override
 	public URLModel execute(HttpServletRequest request)
 			throws ServletException, IOException, DatabaseConnectException, DatabaseQueryException {
-		// TODO Auto-generated method stub
-		return null;
+		try {int commentId = Integer.parseInt(request.getParameter("commentId"));
+			
+			new CommentService().deleteComment(commentId);
+			
+			request.setAttribute("result", new ResponseModel(201, "deleted"));
+		
+		} catch (DatabaseConnectException e) {
+			throw new RuntimeException(e);
+		}
+		
+		return new URLModel();
 	}
 
 }
